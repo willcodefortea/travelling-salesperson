24506eba28aae89f6150fcb471dfa7d4d48bdb23
Ben <willcodefortea@gmail.com>
2022-10-16 11:22:02 +0100

    Extend solution interface to support mutations

--- src/optimisation/search/solution.ts
+++ src/optimisation/search/solution.ts
@@ -18,6 +18,24 @@ class Solution {
     const trip = new Trip([...this.trip.cities]);
     return new Solution(this.cities, trip);
   }
+
+  cost() {
+    return this.trip.objectiveFunction();
+  }
+
+  swap(a: number, b: number): Solution {
+    const tripCities = [...this.trip.cities];
+    [tripCities[b], tripCities[a]] = [tripCities[a], tripCities[b]];
+    const trip = new Trip(tripCities);
+    return new Solution(this.cities, trip);
+  }
+
+  moveSubRoute(fromIdx: number, toIdx: number, n: number): Solution {
+    let newRoute = [...this.trip.cities];
+    newRoute.splice(toIdx, 0, ...newRoute.splice(fromIdx, n));
+    const trip = new Trip(newRoute);
+    return new Solution(this.cities, trip);
+  }
 }
 
 export default Solution;
